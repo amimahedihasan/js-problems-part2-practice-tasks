e77127e6d903ce3d07dc25f23dc170b895f70176
Md Mahedi Hasan <mdmahedi6427@gmail.com>
2026-02-05 13:22:39 +0600

    updated task1

--- task1.js
+++ task1.js
@@ -1,3 +1,8 @@
+// Task -1:
+// Find the lowest number in the array below.
+// const heights2 = [167, 190, 120, 165, 137];
+
+
 const heights2 = [167, 190, 120, 165, 137];
 function smallestNumber (array){
     let min = array[0]
